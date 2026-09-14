051ec13f274d04b052b59787a3d15cbf23722558
Pedro Henrique Baliza Ramos <pedrobalizar@gmail.com>
2026-09-14 18:49:20 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Insira um número inteiro: ");   
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.print("Insira um número: ");
+            int numero = scanner.nextInt();
+
+            if(numero == 0){
+                System.out.println("Programa encerrado");
+                break;
+            }
+
+            if(numero < 0){
+                System.out.println("insira um número maior que 0");
+            }
+
+            else{
+                for(int i = 0, i <= 10, i++){
+                    int resultado = numero * i;
+
+                    if(resultado % 2 == 0){
+                        System.out.println(numero + " x " + i + " = " + resultado + " (par)");
+                    }
+                    else{
+                        System.out.println(numero + " x " + i + " = " + resultado + " (ímpar)");
+                    }
+                }
+            }
+
+            
+        } 
+        scanner.close();  
     }
 }
